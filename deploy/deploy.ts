57efa492055faeb88e6b7bbd64d60aca0aad5806
alexwanga-byte <mdmudazg0388@outlook.com>
2025-12-24 11:55:53 +0800

    Update deploy script for DarkFiber

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedDarkFiber = await deploy("DarkFiber", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`DarkFiber contract: `, deployedDarkFiber.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_darkFiber"; // id required to prevent reexecution
+func.tags = ["DarkFiber"];
